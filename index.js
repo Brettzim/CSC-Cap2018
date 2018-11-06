95b964d60380fc65c3511b98cc2d626bed515227
JohnAVictor <john1@Johns-MacBook-Air-2.local>
2018-11-05 22:57:37 -0500

    upload sample data and isolate variables

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const data1 = require('./data.json');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var zipcode1 = [];
+var reason1 = [];
+
+for (var i=0; i<data1.d.length; i++){
+    var zipcode = data1.d[i].POSTCODE;
+    var reason = data1.d[i]["PRIMARY VACATE REASON"];
+
+    zipcode1.push(zipcode);
+    reason1.push(reason);
+};
+
+// console.log(zipcode1);
+// console.log(reason1);
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+var unique_zipcode1 = zipcode1.filter(onlyUnique);
+var unique_reason1 = reason1.filter(onlyUnique);
+
+console.log(unique_zipcode1);
+console.log(unique_reason1);
+
+app.get('/', (req, res) => res.send(unique_reason1));
+//app.get('/crimes', (req,res) => res.send());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
