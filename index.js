769b2c034575841ef12850d48f7c4f64d9d930fa
brettZim <bz8583a@student.american.edu>
2018-12-09 16:11:51 -0500

    change from nyc to DC

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data1 = require('./data.json');
+const dc_data = require('./dcdata.json');
 
-var zipcode1 = [];
-var reason1 = [];
+var dc_data_list = [];
 
-for (var i=0; i<data1.d.length; i++){
-    var zipcode = data1.d[i].POSTCODE;
-    var reason = data1.d[i]["PRIMARY VACATE REASON"];
+let i = 0;
 
-    zipcode1.push(zipcode);
-    reason1.push(reason);
-};
+for (dc_data["neighborhood_cluster"] in dc_data) {
+ //   let diversity_score = 100;
+    var dc = dc_data[i];
 
-// console.log(zipcode1);
-// console.log(reason1);
 
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
+        // get all 4 numbers and subtract them from each other (absolute value), then subtract that value from 100
+    let total = 0;
+    let score = [];
+    // let score = new Object();
+    //let score = {};
+    let diversity_score = 100;
+
+    for (i = 0; i < dc_data_list.length; i++) {
+        total = Math.abs(dc_data[i].Pct_black - dc_data[i].Pct_white);
+        total = Math.abs(total - dc_data[i].Pct_Asian_PacificIslander);
+        total = Math.abs(total - dc_data[i].Pct_Hispanic);
+        diversity_score -= total;
+        score.push(diversity_score);
+        //score.diversity_score = diversity_score;
+        // let score = {
+        //     diversity_score : diversity_score
+        // };
+        diversity_score = 100;
+    };
+    console.log(score);
+
+    var dc = {
+        neighborhood_cluster : dc_data[i]["neighborhood_cluster"],
+        pct_black : dc_data[i]["Pct_black"],
+        pct_white : dc_data[i]["Pct_white"],
+        pct_hispanic : dc_data[i]["Pct_Hispanic"],
+        pct_asian_pi : dc_data[i]["Pct_Asian_PacificIslander"],
+        score : score[i]//.diversity_score]
+    };
+    dc_data_list.push(dc);
+    i++;
 }
 
-var unique_zipcode1 = zipcode1.filter(onlyUnique);
-var unique_reason1 = reason1.filter(onlyUnique);
 
-console.log(unique_zipcode1);
-console.log(unique_reason1);
+ console.log("this is our demographic array: ", dc_data_list);
 
-app.get('/', (req, res) => res.send(unique_reason1));
-//app.get('/crimes', (req,res) => res.send());
+app.get('/demographics', (req, res) => res.send(dc_data_list));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i=0; i < dc_data.length; i++){
+//     var dcinfo = dc_data[i];//["JURISDICTION NAME"];
+
+//     dc_data_list.push(dcinfo);
+// };
