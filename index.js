6b3118180badb649be0c9f9f8033ceb7e4582f3e
brettZim <bz8583a@student.american.edu>
2018-12-11 20:40:37 -0500

    everything but home page complete

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const port = 3000;
 const dc_data = require('./dcdata.json');
 const dc_data1 = require('./dcdata1.json');
 const dc_data2 = require('./dcdata2.json');
+const dc_data3 = require('./dcdata3.json');
 
 var ds = require('./diversity.js');
 var cs = require('./crime.js');
 var es = require('./economics.js');
-//var fs = require('./finalscores.js');
+var fs = require('./finalscores.js');
 
-var finaldiversityscore = cs(dc_data2);
-var finalcrimescore = ds(dc_data);
-var finaleconomicscore = es(dc_data1);
+var finaldiversityscore = cs(dc_data);
+var finalcrimescore = ds(dc_data1);
+var finaleconomicscore = es(dc_data2);
+//var finalscore = fs(dc_data3);
 
+//console.log(fs);
+
+//app.get('/', (req, res) => res.send(finalscore));
 app.get('/diversity', (req, res) => res.send(finaldiversityscore));
 app.get('/crime', (req, res) => res.send(finalcrimescore));
 app.get('/economics', (req, res) => res.send(finaleconomicscore));
